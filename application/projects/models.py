340a32648db763a56589364808bd9d6a1e8f7381
Yusif Salam-zade <yusalam@gmail.com>
2019-09-26 14:14:17 +0300

    Only project owner can mark it complete or delete the project.

--- application/projects/models.py
+++ application/projects/models.py
@@ -1,5 +1,6 @@
 from application import db
 from application.models import Base
+from sqlalchemy.sql import text
 
 
 class Project(Base):
@@ -11,3 +12,13 @@ class Project(Base):
     def __init__(self, name):
         self.name = name
         self.completed = False
+
+    @staticmethod
+    def get_owner(self):
+        sql_stmt = text("SELECT DISTINCT Account.name FROM Account "
+                        "LEFT JOIN Project ON Project.account_id = Account.id "
+                        "WHERE Account.id = :account_id ").params(account_id=self.account_id)
+        res = db.engine.execute(sql_stmt)
+        print("NOW PRINTING OWNER")
+        return res.first()[0]
+
