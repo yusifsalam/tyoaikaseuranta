f4ccc17bb43cd4e4295c382fdfc75eb9a88de3af
Yusif Salam-zade <yusalam@gmail.com>
2019-10-03 23:05:55 +0300

    Start implementing user roles

--- application/__init__.py
+++ application/__init__.py
@@ -13,6 +13,50 @@ else:
     app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
+
+from os import urandom
+
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager, current_user
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = 'auth_login'
+login_manager.login_message = 'Please login to use this functionality'
+from functools import wraps
+
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+
+            if not current_user.is_authenticated:
+                return login_manager.unauthorized()
+
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = True
+
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+
+            return fn(*args, **kwargs)
+
+        return decorated_view
+
+    return wrapper
+
 from application import views
 
 from application.projects import models
@@ -25,17 +69,6 @@ from application.tasks import models
 from application.tasks import views
 
 from application.auth.models import User
-from os import urandom
-
-app.config["SECRET_KEY"] = urandom(32)
-
-from flask_login import LoginManager
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-login_manager.login_view = 'auth_login'
-login_manager.login_message = 'Please login to use this functionality'
 
 
 @login_manager.user_loader
