2e1a84cbb3ce5f38855583d597f291bc427d3dd3
Yusif Salam-zade <yusalam@gmail.com>
2019-10-13 15:35:06 +0300

    implement ability to add tasks to projects

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -13,3 +13,28 @@ def tasks_index(project_id):
     project = Project.query.get(project_id)
     tasks = project.tasks
     return render_template('tasks/task_list.html', tasks=tasks)
+
+
+@app.route('/projects/<int:project_id>/tasks/new')
+@login_required
+def tasks_form(project_id):
+    return render_template('tasks/new_task.html', form=TaskForm(), project_id=project_id)
+
+
+@app.route('/projects/<int:project_id>/new_task', methods=['POST'])
+@login_required
+def tasks_create(project_id):
+    project = Project.query.get(project_id)
+    form = TaskForm(request.form)
+    if not form.validate():
+        return render_template('tasks/new_task.html', form=form, project_id=project_id)
+    new_task = Task(form.category.data, form.hours.data, form.description.data)
+    new_task.project_id = project.id
+    new_task.account_id = current_user.id
+    current_user.tasks.append(new_task)
+    project.tasks.append(new_task)
+    db.session().add(new_task)
+    db.session().add(current_user)
+    db.session().add(project)
+    db.session().commit()
+    return redirect(url_for('tasks_index', project_id=project_id))
